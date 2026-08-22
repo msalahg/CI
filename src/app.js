82e0e2291f4ff023edd85a25550e3f26406579a5
Mahmoud Salah <mahmoudsghrably@gmail.com>
2026-08-22 15:57:13 +0300

    Add comment to greet function

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+// This is a simple Node.js application that exports a greet function. The greet function takes a name as an argument and returns a greeting message. If the script is run directly from the command line, it will greet the name provided as an argument or default to "World" if no name is given.
+
 function greet(name) {
     return `Hello, ${name}!`;
 }
